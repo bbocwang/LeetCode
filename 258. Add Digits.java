d9c4ec9b8a0081e681230a5473e182bf0e0de52c
wonghao <s1787269@ed.ac.uk>
2018-01-30 23:03:15 +0000

    Update 258. Add Digits.java

--- 258. Add Digits.java
+++ 258. Add Digits.java
@@ -15,3 +15,12 @@ class Solution {
         return Integer.toString(res);
     }
 }
+
+
+
+
+class Solution {
+    public int addDigits(int num) {
+        return num == 0 ? 0 : (num % 9 == 0 ? 9 : (num % 9));
+    }
+}
