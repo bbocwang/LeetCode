269c5a714f103423d67bcf1f27617e97bc39ef81
Hao Wang <s1787269@ed.ac.uk>
2018-10-26 22:02:53 +0100

    Create 136SingleNumber.py

---
+++ 136SingleNumber.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dic={}
+        for num in nums:
+            dic[num]=dic.get(num,0)+1
+        for key,val in dic.items():
+            if val==1: 
+                return key
+                
+        def singleNumber2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return 2*sum(set(nums))-sum(nums)
+                
